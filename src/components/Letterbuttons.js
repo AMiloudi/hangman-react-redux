7ae637aba2eff4e806f3c08164ee41ed0f1e0db8
ANISSA <anissamiloudi@hotmail.nl>
2018-02-10 11:40:53 +0100

    some nice buttons on the screen

---
+++ src/components/Letterbuttons.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types'
+
+const alphabet = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+                  'p','q','r','s','t','u','v','w','x', 'y', 'z']
+
+class LetterButton extends PureComponent {
+
+render() {
+  return (
+    alphabet.map((letter, index)=>
+       <button>{letter}</button>
+    )
+  )
+}
+}
+
+export default LetterButton
